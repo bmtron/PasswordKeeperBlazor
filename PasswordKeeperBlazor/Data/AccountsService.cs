48e16ba48bea27669070c8d49149c99dd8d6eacd
bmtron <bmmeehan3@gmail.com>
2023-05-15 23:27:52 -0400

    Working EF model and base razor page with idenity authentication

---
+++ PasswordKeeperBlazor/Data/AccountsService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using AccountsModel;
+namespace PasswordKeeperBlazor.Data
+{
+	public class AccountsService
+	{
+        AccountsModel.AccountsModel db;
+
+        public AccountsService()
+		{
+            db = new AccountsModel.AccountsModel();
+
+        }
+        public List<AccountLogin> GetAccounts()
+        {
+            var x = db.AccountLogins.ToList();
+
+            return x;
+        }
+
+        public List<AccountLogin> GetAccountsByMasterId(int masterId)
+        {
+            return db.AccountLogins.Where(al => al.MasterAccount.MasterId == masterId).ToList();
+        }
+
+        public int GetMasterId(string aspUserId)
+        {
+            var masterId = db.MasterAccounts.Single(ma => ma.AspUserID == aspUserId);
+            return masterId.MasterId;
+        }
+    }
+}
+
--- PasswordKeeperBlazor/Program.cs
+++ PasswordKeeperBlazor/Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services
     .AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+builder.Services.AddScoped<AccountsService>();
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
